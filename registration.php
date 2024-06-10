2c2a26cb5d94a8f36b05e393874d847cbbf91682
FBatrisyia <115613270+FBatrisyia@users.noreply.github.com>
2024-06-10 16:29:56 +0800

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> MMU's Book Sharing Center: Register Now</title>
+    </head>
+    <body>
+        <h1> Logo </h1>
+        
+        <p><label for = "studentID"> Student ID </label></p>
+        <p><input type = "number" id = "studentID" required/></p>
+
+        <p><label for = "name"> Name </label></p>
+        <p><input type = "text" id = "name" required/></p>
+
+        <p><label for = "email"> Email </label></p>
+        <p><input type = "email" id = "email" required/></p>
+
+        <p><label for = "password"> Password </label></p>
+        <p><input type = "password" id = "password" required/></p>
+
+        
+        
+        
+    </body>
+</html>
