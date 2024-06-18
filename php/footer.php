657b5ba98b18d30aeafbe82ec784b31056f7b7f3
JyckoS <jyckosianjaya@gmail.com>
2024-06-19 00:05:33 +0800

    Footer Page

---
+++ php/footer.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<!-- BY JYCKO -->
+<head>
+<link rel="stylesheet" href="../css/footerstyle.css">
+
+    <link rel="stylesheet" href="css/footerstyle.css">
+</head>
+<body>
+    <footer>
+        <div class="footerstyle">
+            <!-- Short description about mmu book sharing-->
+            <div>
+                <h4>About MMU Book Sharing</h4>
+                <p>We help MMU Students to share and exchange books.</p>
+            </div>
+
+
+            <!-- Navigation to Other pages -->
+            <div>
+                <h4>Navigation</h4>
+                <a href="../index.php">Home</a>
+                <a href="about.php">About Us</a>
+                <a href="faq.php">FAQs</a>
+            </div>
+
+
+            <!-- MMU book copyright -->
+            <div>
+                <p> MMU Book Sharing. All rights belongs to MMU.
+                </p>
+            </div>
+        </div>
+        
+    </footer>
+</body>
+
+</html>
