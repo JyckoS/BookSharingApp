88ba0ea0f73555a943b0f1bdf0f3f191fc6ac4e4
JyckoS <jyckosianjaya@gmail.com>
2024-06-19 00:05:21 +0800

    About Us Page

---
+++ php/aboutus.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MMU Book Sharing - About Us</title>
+    </head>
+    <body>
+
+        <!-- TITLE -->
+        <div>
+            <h1>MMU Book Sharing</h1>
+            <h4>Exchange and share books</h4>
+            <hr>
+</div>
+        <article>
+            <h1>What is MMU Book Sharing?</h1>
+            <p>Founded in 2024, MMUBookSharing.com is the home to the community for MMU Students
+                where they are able to share the books they have and borrow books they need.
+                We provide many features to ensure the best quality and the highest comfort so
+                students are able to share and borrow books with ease.</p>
+            <hr>
+            <h1>Loaning Books</h1>
+            <p>
+                You are able to loan your books, not only just to give them away.
+                If you have a book that you want to share but be returned back after a certain
+                period of time. You can loan it.
+            </p>
+            
+            <hr>
+
+            <h1>Requesting Books</h1>
+            <p>We understand that sometimes the book you are looking for are not available
+                in our inventory. In that case, you are able to request for books from the MMU
+                community. You can request books in This forum. [Later change link]
+            </p>
+
+
+        </article>
+        <?php
+        include "footer.php";
+        ?>
+    </body>
+</html>
