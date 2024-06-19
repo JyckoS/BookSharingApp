7563397fbf08a79f4fc7c0f9a37ed99fcba455fe
JyckoS <jyckosianjaya@gmail.com>
2024-06-19 09:14:11 +0800

    Footer design improve

--- php/footer.php
+++ php/footer.php
@@ -11,7 +11,7 @@
         <div class="footerstyle">
             <!-- Short description about mmu book sharing-->
             <div>
-                <h4>About MMU Book Sharing</h4>
+                <h4>MMU Book Sharing</h4>
                 <p>We help MMU Students to share and exchange books.</p>
             </div>
 
@@ -20,14 +20,14 @@
             <div>
                 <h4>Navigation</h4>
                 <a href="../index.php">Home</a>
-                <a href="about.php">About Us</a>
+                <a href="aboutus.php">About Us</a>
                 <a href="faq.php">FAQs</a>
             </div>
 
 
             <!-- MMU book copyright -->
             <div>
-                <p> MMU Book Sharing. All rights belongs to MMU.
+                <p class="copyright">&copy; MMU Book Sharing. All rights belongs to MMU.
                 </p>
             </div>
         </div>
