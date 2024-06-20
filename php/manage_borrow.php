8a1eecabd5a4d6f24a5bc9a159362426ca31d121
ximrui <1201101479@student.mmu.edu.my>
2024-06-21 02:57:20 +0800

    fix and formatting

--- php/manage_borrow.php
+++ php/manage_borrow.php
@@ -10,12 +10,13 @@ $connection = openConnection();
 
 // Pagination setup
 $limit = 5; // Number of entries to show in a page.
-if (isset($_GET["page"])) { 
-    $page  = $_GET["page"]; 
-} else { 
-    $page = 1; 
-};  
-$start_from = ($page-1) * $limit;
+if (isset($_GET["page"])) {
+    $page = $_GET["page"];
+} else {
+    $page = 1;
+}
+;
+$start_from = ($page - 1) * $limit;
 
 // Fetch data from borrow table
 $sql = "SELECT BorrowID, BookID, StudentID, BorrowDate, ReturnDate FROM borrow LIMIT $start_from, $limit";
@@ -31,6 +32,7 @@ $total_pages = ceil($total_records / $limit);
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <link rel="stylesheet" href="../css/footerstyle.css">
     <link rel="stylesheet" href="../css/headerstyle.css">
@@ -39,59 +41,47 @@ $total_pages = ceil($total_records / $limit);
 
     <title>Manage Borrow - Book Sharings App MMU</title>
 </head>
+
 <body>
-<?php
+    <?php
     include "includes/header.php";
-?>
-<div class="content">
-    <h2>Manage Borrow</h2>
-    <a href="manager.php" class="button">Back</a>
-    <div class="table-container">
-        <table>
-            <tr>
-                <th>BorrowID</th>
-                <th>BookID</th>
-                <th>StudentID</th>
-                <th>BorrowDate</th>
-                <th>ReturnDate</th>
-            </tr>
-            <?php while($row = mysqli_fetch_assoc($result)) { ?>
+    ?>
+    <div class="content">
+        <h2>Manage Borrow</h2>
+        <a href="manager.php" class="button">Back</a>
+        <div class="table-container">
+            <table>
                 <tr>
-                    <td><?php echo $row['BorrowID']; ?></td>
-                    <td><?php echo $row['BookID']; ?></td>
-                    <td><?php echo $row['StudentID']; ?></td>
-                    <td><?php echo $row['BorrowDate']; ?></td>
-                    <td><?php echo $row['ReturnDate']; ?></td>
+                    <th>BorrowID</th>
+                    <th>BookID</th>
+                    <th>StudentID</th>
+                    <th>BorrowDate</th>
+                    <th>ReturnDate</th>
                 </tr>
-            <?php } ?>
-        </table>
-    </div>
-    <div class="pagination">
-        <?php
-        for ($i=1; $i<=$total_pages; $i++) { 
-            echo "<a href='manage_borrow.php?page=".$i."' class='".($page == $i ? "active" : "")."'>".$i."</a> "; 
-        }
-        ?>
-    </div>
-</div>
-<footer>
-    <div class="footerstyle">
-        <div>
-            <h4>MMU Book Sharing</h4>
-            <p>We help MMU Students to share and exchange books.</p>
+                <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+                    <tr>
+                        <td><?php echo $row['BorrowID']; ?></td>
+                        <td><?php echo $row['BookID']; ?></td>
+                        <td><?php echo $row['StudentID']; ?></td>
+                        <td><?php echo $row['BorrowDate']; ?></td>
+                        <td><?php echo $row['ReturnDate']; ?></td>
+                    </tr>
+                <?php } ?>
+            </table>
         </div>
-        <div>
-            <h4>Navigation</h4>
-            <a href="../index.php">Home</a>
-            <a href="aboutus.php">About Us</a>
-            <a href="faq.php">FAQs</a>
-        </div>
-        <div>
-            <p class="copyright">&copy; MMU Book Sharing. All rights belong to MMU.</p>
+        <div class="pagination">
+            <?php
+            for ($i = 1; $i <= $total_pages; $i++) {
+                echo "<a href='manage_borrow.php?page=" . $i . "' class='" . ($page == $i ? "active" : "") . "'>" . $i . "</a> ";
+            }
+            ?>
         </div>
     </div>
-</footer>
+    <?php
+    include "includes/footer.php";
+    ?>
 </body>
+
 </html>
 <?php
 closeConnection($connection);
