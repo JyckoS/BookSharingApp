f3b64a0cc510e2d94352ecb49aace873f2e3b233
ximrui <1201101479@student.mmu.edu.my>
2024-06-21 03:09:38 +0800

    fix

--- php/manage_loan.php
+++ php/manage_loan.php
@@ -10,13 +10,12 @@ $connection = openConnection();
 
 // Pagination setup
 $limit = 5; // Number of entries to show in a page.
-if (isset($_GET["page"])) {
-    $page = $_GET["page"];
-} else {
-    $page = 1;
-}
-;
-$start_from = ($page - 1) * $limit;
+if (isset($_GET["page"])) { 
+    $page  = $_GET["page"]; 
+} else { 
+    $page = 1; 
+};  
+$start_from = ($page-1) * $limit;
 
 // Fetch data from loan table
 $sql = "SELECT LoanID, StudentID, LoanDate, ExpiryDate FROM loan LIMIT $start_from, $limit";
@@ -32,7 +31,6 @@ $total_pages = ceil($total_records / $limit);
 
 <!DOCTYPE html>
 <html>
-
 <head>
     <link rel="stylesheet" href="../css/footerstyle.css">
     <link rel="stylesheet" href="../css/headerstyle.css">
@@ -41,45 +39,57 @@ $total_pages = ceil($total_records / $limit);
 
     <title>Manage Loans - Book Sharings App MMU</title>
 </head>
-
 <body>
-    <?php
+<?php
     include "includes/header.php";
-    ?>
-    <div class="content">
-        <h2>Manage Loans</h2>
-        <a href="manager.php" class="button">Back</a>
-        <div class="table-container">
-            <table>
+?>
+<div class="content">
+    <h2>Manage Loans</h2>
+    <a href="manager.php" class="button">Back</a>
+    <div class="table-container">
+        <table>
+            <tr>
+                <th>LoanID</th>
+                <th>StudentID</th>
+                <th>LoanDate</th>
+                <th>ExpiryDate</th>
+            </tr>
+            <?php while($row = mysqli_fetch_assoc($result)) { ?>
                 <tr>
-                    <th>LoanID</th>
-                    <th>StudentID</th>
-                    <th>LoanDate</th>
-                    <th>ExpiryDate</th>
+                    <td><?php echo $row['LoanID']; ?></td>
+                    <td><?php echo $row['StudentID']; ?></td>
+                    <td><?php echo $row['LoanDate']; ?></td>
+                    <td><?php echo $row['ExpiryDate']; ?></td>
                 </tr>
-                <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-                    <tr>
-                        <td><?php echo $row['LoanID']; ?></td>
-                        <td><?php echo $row['StudentID']; ?></td>
-                        <td><?php echo $row['LoanDate']; ?></td>
-                        <td><?php echo $row['ExpiryDate']; ?></td>
-                    </tr>
-                <?php } ?>
-            </table>
+            <?php } ?>
+        </table>
+    </div>
+    <div class="pagination">
+        <?php
+        for ($i=1; $i<=$total_pages; $i++) { 
+            echo "<a href='manage_loans.php?page=".$i."' class='".($page == $i ? "active" : "")."'>".$i."</a> "; 
+        }
+        ?>
+    </div>
+</div>
+<footer>
+    <div class="footerstyle">
+        <div>
+            <h4>MMU Book Sharing</h4>
+            <p>We help MMU Students to share and exchange books.</p>
         </div>
-        <div class="pagination">
-            <?php
-            for ($i = 1; $i <= $total_pages; $i++) {
-                echo "<a href='manage_loans.php?page=" . $i . "' class='" . ($page == $i ? "active" : "") . "'>" . $i . "</a> ";
-            }
-            ?>
+        <div>
+            <h4>Navigation</h4>
+            <a href="../index.php">Home</a>
+            <a href="aboutus.php">About Us</a>
+            <a href="faq.php">FAQs</a>
+        </div>
+        <div>
+            <p class="copyright">&copy; MMU Book Sharing. All rights belong to MMU.</p>
         </div>
     </div>
-    <?php
-    include "includes/footer.php";
-    ?>
+</footer>
 </body>
-
 </html>
 <?php
 closeConnection($connection);
