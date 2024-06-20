b077da2cf0d9130c95c4b5c7cb6e7ae7749cdf24
ximrui <1201101479@student.mmu.edu.my>
2024-06-21 03:15:09 +0800

    Revert "fix"
    This reverts commit 04607fd61013447515c296725a4705d396e201b1.

--- php/manager.php
+++ php/manager.php
@@ -11,30 +11,41 @@ $connection = openConnection();
 
 <!DOCTYPE html>
 <html>
-
 <head>
     <link rel="stylesheet" href="../css/footerstyle.css">
     <link rel="stylesheet" href="../css/headerstyle.css">
-    <link rel="stylesheet" href="../css/button.css">
+    <link rel="stylesheet" href="../css/button.css"> 
     <title>Manager Panel - Book Sharings App MMU</title>
 </head>
-
 <body>
-    <?php
+<?php
     include "includes/header.php";
-    ?>
-    <div class="content">
-        <h2>Welcome, Manager!</h2>
-        <div class="button-container">
-            <a href="manage_borrow.php" class="square-button">Manage Borrow</a>
-            <a href="manage_loan.php" class="square-button">Manage Loans</a>
+?>
+<div class="content">
+    <h2>Welcome, Manager!</h2>
+    <div class="button-container">
+        <a href="manage_borrow.php" class="square-button">Manage Borrow</a>
+        <a href="manage_loan.php" class="square-button">Manage Loans</a>
+    </div>
+</div>
+<footer>
+    <div class="footerstyle">
+        <div>
+            <h4>MMU Book Sharing</h4>
+            <p>We help MMU Students to share and exchange books.</p>
+        </div>
+        <div>
+            <h4>Navigation</h4>
+            <a href="../index.php">Home</a>
+            <a href="aboutus.php">About Us</a>
+            <a href="faq.php">FAQs</a>
+        </div>
+        <div>
+            <p class="copyright">&copy; MMU Book Sharing. All rights belong to MMU.</p>
         </div>
     </div>
-    <?php
-    include "includes/footer.php";
-    ?>
+</footer>
 </body>
-
 </html>
 <?php
 closeConnection($connection);
