b651aecdfef91f73ee84dd8d6286954ca162a14e
JyckoS <jyckosianjaya@gmail.com>
2024-06-22 18:22:52 +0800

    Finish loan books

--- php/actions/process_loan.php
+++ php/actions/process_loan.php
@@ -1,37 +1,62 @@
 <?php
+// Made by Jycko & Batrisyia
+require_once "../db_connect.php";
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    header("Location: ../loanbooks.php?message=error");
+    exit;
+}
+echo "wafaw";
 
-    session_start();
+session_start();
+$conn = openConnection();
+$title = mysqli_real_escape_string($conn, $_POST['title']);
+$author = mysqli_real_escape_string($conn, $_POST['author']);
+$genre = mysqli_real_escape_string($conn, $_POST['genre']);
+$description = mysqli_real_escape_string($conn, $_POST['description']);
+$publisher = mysqli_real_escape_string($conn, $_POST['publisher']);
+$yearPublished = mysqli_real_escape_string($conn, $_POST['yearPublished']);
+$bookCondition = mysqli_real_escape_string($conn, $_POST['bookCondition']);
+$startDate = mysqli_real_escape_string($conn, $_POST['startDate']);
+$endDate = mysqli_real_escape_string($conn, $_POST['endDate']);
+$userID = $_SESSION["userid"];
+echo "test";
+if (empty($title) || empty($author) || empty($genre) || empty($description) ||
+empty($publisher) || empty($yearPublished) || empty($bookCondition) || empty($startDate
+|| empty($endDate))) {
+    header("Location: ../loanbooks.php?message=empty");
+    exit;
+}
 
-    require "db_connect.php";
-    $connection = openConnection();
+$query = "INSERT INTO loan (StudentID, LoanDate, ExpiryDate) 
+VALUES
+('$userID', '$startDate', '$endDate');";
 
-    $userid = $_SESSION["userid"];
-    
-    if($_SERVER["REQUEST_METHOD"] == "POST")
-    {
-        $bookTitle = $_POST["bookTitle"];
-        $author = $_POST["author"];
-        $description = $_POST["description"];
-        $genre = $_POST["genre"];
-        $publisher = $_POST["publisher"];
-        $yearPublished = $_POST["yearPublished"];
-        $bookCondition = $_POST["bookCondition"];
-        $startDate = $_POST["startDate"];
-        $endDate = $_POST["endDate"];
+$insert = mysqli_query($conn, $query);
+echo "vvv";
 
-        try{
-            $query = "INSERT INTO books (Title, Author, Publisher, 
-            YearPublished, Genre, BookCondition, Description, Status, StudentID) VALUES 
-            ('$title', '$author', '$publisher', '$yearPublished', '$genre', '$bookCondition', '$description', 'AVAILABLE', '$userID')";
 
-        }catch(PDOException $e){
-            die("Query failed: " . $e->getMessage());
-        }
-    }
-    else
-    {
-        header("Location:../index.php");
-    }
-    
+if (!$insert) {
+    header("Location: ../loanbooks.php?message=sqlerror1");
+    exit;
+}
+$loanID = mysqli_insert_id($conn);
+
+echo "xxxx";
+
+$query = "INSERT INTO books 
+(LoanID, Title, Author, Genre, Description, Publisher, YearPublished, BookCondition, Status, StudentID) 
+VALUES 
+('$loanID', '$title', '$author', '$genre', '$description', '$publisher', '$yearPublished', '$bookCondition', 'PENDING', '$userID')";
+echo "zzzz";
+
+$insert = mysqli_query($conn, $query);
+echo "ppp";
+
+if (!$insert) {
+    header("Location: ../loanbooks.php?message=sqlerror2");
+    exit;
+}
+header("Location: ../loanbooks.php?message=success");
+exit;
 
 ?>
--- php/loanbooks.php
+++ php/loanbooks.php
@@ -1,63 +1,74 @@
-<?php
-
-
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
         <title> MMU's Book Sharing: Loan Books </title>
         <link rel = "stylesheet" href = "../css/loanbooks.css"/>
     </head>
+    <?php
+        include 'includes/header.php';
+    ?>
     <body>
         <h3> Loan Books </h3>
 
         <p> Please fill in the form, if you're liked to loan books to other students. </p>
-        <p> Please note that your book may take some to get approved by the manager of this website. </p>
+        <p> Please note that your book may take some time to get approved by the manager of this website. </p>
         
-        <form action = "actions/process_login.php" method = "post">
+        <form action = "actions/process_loan.php" method = "post">
             <p> Book Information </p>
 
             <p><label for = "title"> Book Title </label></p>
-            <p><input type = "text" id = "title" name = "title" placeholder = "Title"/></p>
+            <p><input type = "text" id = "title" name = "title" placeholder = "Title" required/></p>
                 
             <p><label for = "author"> Author </label></p>
-            <p><input type = "text" id = "author" name = "author" placeholder = "Author"/></p>
+            <p><input type = "text" id = "author" name = "author" placeholder = "Author" required/></p>
 
             <p><label for = "genre"> Genre </label></p>
-            <p><input type = "text" id = "genre" name = "genre" placeholder = "Genre"/></p>
+            <p>
+                <select id = "genre" name = "genre" required>
+                    <option value="">Select Genre</option>
+                    <option value="Fiction">Fiction</option>
+                    <option value="Non-Fiction">Non-Fiction</option>
+                    <option value="Science Fiction">Science Fiction</option>
+                    <option value="Fantasy">Fantasy</option>
+                    <option value="Biography">Biography</option>
+                    <option value="Mystery">Mystery</option>
+                    <option value="Romance">Romance</option>
+                    <option value="History">History</option>
+                    <option value="Other">Others</option>
+                </select>
+            </p>
 
             <p><label for = "description"> Book Description (What the book is about) </label></p>
-            <p><input type = "text" id = "description" name = "description" placeholder = "Description"/></p>
+            <p><input type = "text" id = "description" name = "description" placeholder = "Description" required/></p>
 
             <p><label for = "publisher"> Publisher </label></p>
-            <p><input type = "text" id = "publisher" name = "publisher" placeholder = "Publisher"/></p>
+            <p><input type = "text" id = "publisher" name = "publisher" placeholder = "Publisher" required/></p>
 
             <p><label for = "yearPublished"> Year Published </label></p>
-            <p><input type = "text" id = "yearPublished" name = "yearPublished" placeholder = "Year Published"/></p>
+            <p><input type = "number" id = "yearPublished" name = "yearPublished" placeholder = "Year Published" min="0" step="1" required/></p>
 
             <fieldset>
-            <legend> Book Condition </legend>
+                <legend> Book Condition </legend>
                 <label for = "excellent"> Excellent </label>
-                <input type = "radio" id = "excellent" name = "bookCondition"/>
+                <input type = "radio" id = "excellent" name = "bookCondition" value="EXCELLENT" required/>
 
                 <label for = "bad"> Bad </label>
-                <input type = "radio" id = "bad" name = "bookCondition"/>
+                <input type = "radio" id = "bad" name = "bookCondition" value="BAD" required/>
 
                 <label for = "minorDamages"> Minor Damages </label>
-                <input type = "radio" id = "minorDamages" name = "bookCondition"/>
+                <input type = "radio" id = "minorDamages" name = "bookCondition" value="MINOR_DAMAGES" required/>
 
                 <label for = "missingPages"> Missing Pages </label>
-                <input type = "radio" id = "missingPages" name = "bookCondition"/>
+                <input type = "radio" id = "missingPages" name = "bookCondition" value="MISSING_PAGES" required/>
             </fieldset>
 
-            <p> Duration of Loan <p>
+            <p> Duration of Loan </p>
             <p>
                 <label for = "startDate"> Start of Loan </label>
-                <input type = "date" id = "startDate" name = "startDate"/>
+                <input type = "date" id = "startDate" name = "startDate" required/>
 
                 <label for = "endDate"> End of Loan </label>
-                <input type = "date" id = "endDate" name = "endDate"/>
+                <input type = "date" id = "endDate" name = "endDate" required/>
             </p>
 
             <div class = "terms"> 
@@ -67,12 +78,13 @@
                     to other students and the website manager. Disputes should be resolved via our mediation service. Our platform 
                     facilitates loans but does not assume liability for transaction issues. Continued use signifies acceptance of 
                     updated terms.</p>
-
             </div>
 
             <input type = "submit" value = "Submit"/>
 
         </form>
-
+        <?php
+            include 'includes/footer.php';
+        ?>
     </body>
 </html>
