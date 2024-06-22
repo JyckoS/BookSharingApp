3392bc57cdacb4041363827814b10087b7575e55
JyckoS <jyckosianjaya@gmail.com>
2024-06-22 20:28:12 +0800

    Allow manager to see feedbacks

--- php/manager.php
+++ php/manager.php
@@ -28,6 +28,8 @@ $connection = openConnection();
         <div class="button-container">
             <a href="manage_borrow.php" class="square-button">Manage Borrow</a>
             <a href="manage_loan.php" class="square-button">Manage Loans</a>
+            <a href="manager_feedback.php" class="square-button">See Feedbacks</a>
+
         </div>
     </div>
     <?php
---
+++ php/manager_feedback.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Manager - Read Feedbacks</title>
+    <link rel="stylesheet" href="../css/manager_feedback.css">
+</head>
+
+<body>
+    <?php
+    include "includes/header.php";
+    ?>
+    <article class="padd">
+        <a href="manager.php" class="button">Back</a>
+    <table>
+        <thead>
+            <th>Student Name</th>
+            <th>Student ID</th>
+            <th>Student Email</th>
+
+            <th>Content</th>
+
+        </thead>
+        <tbody>
+            <?php
+                require_once "db_connect.php";
+                $conn = openConnection();
+                $sql = "SELECT * FROM feedback";
+                $result = mysqli_query($conn, $sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $userid = $row["StudentID"];
+                    $userData = getUserRowData($userid);
+                    $content = $row["Content"];
+                    $userName = $userData["Name"];
+                    $email = $userData["Email"];
+                    echo "<tr> 
+                    <td>$userName</td>
+                    <td>$userid</td>
+                    <td>$email</td>
+                    <td>$content</td>
+                    </tr>";
+                }
+            ?>
+        </tbody>
+    </table>
+
+    </article>
+
+    <?php
+    include "includes/footer.php";
+    ?>
+</body>
+
+</html>
